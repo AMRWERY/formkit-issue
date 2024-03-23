e5ae08cab98b95fdc66255b69eb0cb4a3cabe295
AMRWERY <amrmounir2@gmail.com>
2024-03-23 15:25:33 +0200

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,69 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@pinia/nuxt",
+    "@nuxtjs/i18n",
+    "nuxt-icon",
+    "@formkit/nuxt",
+    "vue3-carousel-nuxt",
+  ],
+  tailwindcss: {
+    cssPath: "~/assets/css/tailwind.css",
+    configPath: "tailwind.config",
+    injectPosition: "first",
+    viewer: true,
+  },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  pinia: {
+    storesDirs: ["./stores/**", "./custom-folder/stores/**"],
+  },
+  i18n: {
+    vueI18n: "./i18n.config.ts",
+  },
+  formkit: {
+    autoImport: true,
+    configFile: "./formkit-configs/formkit.config.js",
+  },
+  carousel: {
+    prefix: "MyPrefix",
+  },
+  css: ["./assets/css/main.css"],
+  components: [
+    {
+      path: "~/components",
+      pathPrefix: false,
+    },
+  ],
+  app: {
+    head: {
+      title: "Ship-IT",
+      script: [{}],
+      noscript: [],
+      link: [
+        // {
+        //   rel: "stylesheet",
+        //   href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css",
+        // },
+      ],
+      meta: [
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1",
+        },
+        {
+          charset: "utf-8",
+        },
+      ],
+      // htmlAttrs: { dir: 'ltr' },
+    },
+    pageTransition: { name: "page", mode: "out-in" },
+    layoutTransition: { name: "layout", mode: "out-in" },
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Welcome to Ship-IT</h1>
+  </div>
+</template>
+
+<script setup>
+
+</script>
